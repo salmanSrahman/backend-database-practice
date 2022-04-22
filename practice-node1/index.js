a6a885eeb0b92488fbfda29ae47e2ecbc6d0cb03
Salman-S-Rahman <salmanmern@gmail.com>
2022-04-22 15:30:55 +0600

    find user in expres code

--- practice-node1/index.js
+++ practice-node1/index.js
@@ -2,10 +2,20 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send(
-    "Hello My Lala Jan. How are you? I am your younger brother Taskin Asghar from Afghanistan."
-  );
+const users = [
+  { id: 1, name: "Taskin", no: "017000000" },
+  { id: 2, name: "Asghar", no: "018111111" },
+  { id: 3, name: "Stanikzai", no: "019222222" },
+];
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.get("/user/:id", (req, res) => {
+  console.log(req.params);
+  const id = parseInt(req.params.id);
+  const user = users.find((u) => u.id === id);
+  res.send(user);
 });
 
 app.listen(port, () => {
