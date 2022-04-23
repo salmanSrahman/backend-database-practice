61a0769771e84958b3cc5440a7560127b49c5f01
Salman-S-Rahman <salmanmern@gmail.com>
2022-04-23 23:36:48 +0600

    react with node for user data

--- practice-node1/index.js
+++ practice-node1/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
+var cors = require("cors");
 const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
 
 const users = [
   { id: 1, name: "Taskin", no: "017000000" },
   { id: 2, name: "Asghar", no: "018111111" },
   { id: 3, name: "Stanikzai", no: "019222222" },
+  { id: 4, name: "Hamdullah Mohib", no: "019222222" },
+  { id: 5, name: "Zabiullah Janat", no: "0154444444" },
 ];
 app.get("/users", (req, res) => {
   res.send(users);
@@ -15,6 +20,14 @@ app.get("/user/:id", (req, res) => {
   console.log(req.params);
   const id = parseInt(req.params.id);
   const user = users.find((u) => u.id === id);
+  res.send(user);
+});
+
+app.post("/user", (req, res) => {
+  console.log("request", req.body);
+  const user = req.body;
+  user.id = users.length + 1;
+  users.push(user);
   res.send(user);
 });
 
