d340033709c958c0fd6c299fdeed632820e08c52
Salman-S-Rahman <salmanmern@gmail.com>
2022-04-23 22:30:01 +0600

    data use from server side

--- react-with-node1/src/App.js
+++ react-with-node1/src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   const [users, setUsers] = useState([]);
@@ -7,9 +8,45 @@ function App() {
       .then((res) => res.json())
       .then((data) => setUsers(data));
   }, []);
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    console.log(name, email);
+    const user = { name, email };
+
+    fetch("http://localhost:5000/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const newUser = [...user, data];
+        setUsers(newUser);
+      });
+  };
+
   return (
-    <div>
-      <h1>Users : {users.length}</h1>
+    <div className="App">
+      <form onSubmit={handleFormSubmit}>
+        <input type="name" name="name" placeholder="name" />
+        <br />
+        <br />
+        <input type="email" name="email" placeholder="email" />
+        <br />
+        <br />
+        <input type="submit" value="Add User" />
+        <br />
+      </form>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.id}: {user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
