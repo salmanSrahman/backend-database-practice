019cc70a5228a01cc0efedd3bfe7a07fb362694c
Salman-S-Rahman <salmanmern@gmail.com>
2022-04-30 20:51:13 +0600

    components created

--- react-crud1/src/App.js
+++ react-crud1/src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import AddUser from "./Components/AddUser/AddUser";
+import Home from "./Components/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>React CRUD Operation.</h1>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="user/add" element={<AddUser />}></Route>
+      </Routes>
     </div>
   );
 }
