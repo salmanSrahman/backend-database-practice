4872557e7c93517654b9e7e4a81f01ca56f5ea63
Salman-S-Rahman <salmanmern@gmail.com>
2022-05-01 12:06:28 +0600

    post api from backend

--- node-mongo-crud1/index.js
+++ node-mongo-crud1/index.js
@@ -1,24 +1,41 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+var cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // user : dbuser1
 // password : nfLlwf2GRgzolUrK
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://dbuser1:nfLlwf2GRgzolUrK@cluster0.bgnlt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log("Db conncected");
-  client.close();
-});
+app.use(cors());
+app.use(express.json());
+
+// async function run() {
+//   try {
+//     await client.connect();
+//     const userCollection = client.db("foodExpress").collection("user");
+//     const user = { name: "Salman S. Rahman", email: "salmansr1995@gmail.com" };
+//     const result = await userCollection.insertOne(user);
+//     console.log(`User inserted with ${result.insertedId}`);
+//   } finally {
+//     //     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+async function run() {
+  try {
+   
+    app.post("/user", (req, res) => {
+      const newUser = req.body;
+      console.log("Adding new user", newUser);
+      res.send({ result: "success" });
+    });
+  } finally {
+    //     await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello Taskin Asghar from Bangladesh.");
