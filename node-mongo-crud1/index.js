5b54c4006413b078981e8bb66e247dec3b5eac49
Salman-S-Rahman <salmanmern@gmail.com>
2022-05-01 20:44:20 +0600

    post api from backend

--- node-mongo-crud1/index.js
+++ node-mongo-crud1/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-var cors = require("cors");
+const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // user : dbuser1
@@ -10,25 +10,24 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 app.use(cors());
 app.use(express.json());
 
-// async function run() {
-//   try {
-//     await client.connect();
-//     const userCollection = client.db("foodExpress").collection("user");
-//     const user = { name: "Salman S. Rahman", email: "salmansr1995@gmail.com" };
-//     const result = await userCollection.insertOne(user);
-//     console.log(`User inserted with ${result.insertedId}`);
-//   } finally {
-//     //     await client.close();
-//   }
-// }
-// run().catch(console.dir);
+const uri =
+  "mongodb+srv://dbuser1:nfLlwf2GRgzolUrK@cluster0.bgnlt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
   try {
-   
-    app.post("/user", (req, res) => {
+    await client.connect();
+    const userCollection = client.db("foodExpress").collection("user");
+
+    app.post("/user", async (req, res) => {
       const newUser = req.body;
       console.log("Adding new user", newUser);
+      // const result = await userCollection.insertOne(newUser);
+      // res.send(result);
       res.send({ result: "success" });
     });
   } finally {
