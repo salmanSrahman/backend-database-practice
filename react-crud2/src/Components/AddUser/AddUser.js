52a3b7198984f09ea622e7f85bbd3c56dbea03fd
Salman-S-Rahman <salmanmern@gmail.com>
2022-05-02 03:14:28 +0600

    crud user form used

--- react-crud2/src/Components/AddUser/AddUser.js
+++ react-crud2/src/Components/AddUser/AddUser.js
@@ -1,9 +1,26 @@
 import React from "react";
 
 const AddUser = () => {
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email };
+
+    console.log(user);
+  };
   return (
     <div>
       <h1>Please add user.</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" placeholder="Name" />
+        <br />
+        <br />
+        <input type="email" name="email" placeholder="Email" />
+        <br />
+        <br />
+        <input type="submit" name="submit" value="Add User" />
+      </form>
     </div>
   );
 };
