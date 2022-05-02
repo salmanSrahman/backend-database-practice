457dd577d3c68f1eec7c81527f322710bfde4fa8
Salman-S-Rahman <salmanmern@gmail.com>
2022-05-02 10:08:41 +0600

    userd added from front end & stored data in backend

--- node-mongo-crud2/index.js
+++ node-mongo-crud2/index.js
@@ -2,10 +2,39 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // use middleware
 app.use(cors());
 app.use(express.json());
+
+// user :   dbuser2
+// password :   vBff6PaHILSNxDhA
+
+const uri =
+  "mongodb+srv://dbuser2:vBff6PaHILSNxDhA@cluster0.bgnlt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const userCollection = client.db("foodExpress").collection("user");
+
+    app.post("/user", async (req, res) => {
+      const newUser = req.body;
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+      console.log("User data received", newUser);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Running my node crud server");
