2b8bed67c7ae0d4d35e4f4073b55085f79fd9a61
Salman-S-Rahman <salmanmern@gmail.com>
2022-05-02 14:36:05 +0600

    user deleted

--- react-crud2/src/Components/Home/Home.js
+++ react-crud2/src/Components/Home/Home.js
@@ -8,13 +8,33 @@ const Home = () => {
       .then((data) => setUsers(data));
   }, []);
 
+  const handleDeleteUser = (id) => {
+    const proceed = window.confirm("Are you sure you want to delete?");
+    if (proceed) {
+      console.log("delete this user", id);
+      const url = `http://localhost:5000/user/${id}`;
+      fetch(url, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.deletedCount > 0) {
+            console.log(data);
+            const remaining = users.filter((user) => user._id !== id);
+            setUsers(remaining);
+          }
+        });
+    }
+  };
+
   return (
     <div>
       <h1>Active user : {users.length}</h1>
       <ul>
         {users.map((user) => (
-          <li>
+          <li key={user._id}>
             {user.name} :: {user.email}
+            <button onClick={() => handleDeleteUser(user._id)}>X</button>
           </li>
         ))}
       </ul>
