2862728976e2a31d3d9381c5f0e21558083666f0
Salman-S-Rahman <salmanmern@gmail.com>
2022-05-02 12:16:41 +0600

    user added from front end & stored data in backend

--- react-crud2/src/Components/Home/Home.js
+++ react-crud2/src/Components/Home/Home.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/user")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
   return (
     <div>
-      <h1>This is home.</h1>
+      <h1>Active user : {users.length}</h1>
+      <ul>
+        {users.map((user) => (
+          <li>
+            {user.name} :: {user.email}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
